47384b7bf899ce0382d7610789cb115aed1d3174
JakeLaptop <jakejildefonso@gmail.com>
2018-10-24 13:29:09 -0600

    finished ch5, corrected it, and started ch6

--- ch5/ch5Projects/HiLoGame/HiLoGame/Form1.vb
+++ ch5/ch5Projects/HiLoGame/HiLoGame/Form1.vb
@@ -16,7 +16,7 @@
 
             'get random number
             decRandom = Rnd()
-            rtbResults.AppendText("Pre-calculation result is " & decRandom.ToString & vbNewLine) '<-debug tool
+            rtbResults.AppendText("Pre-Calculation result is " & decRandom.ToString & vbNewLine) '<-debug tool
 
             bytResult = Int((decRandom * 13) + 1)
             'covert to card
@@ -52,7 +52,49 @@
 
     Private Sub btnLow_Click(sender As Object, e As EventArgs) Handles btnLow.Click
         'LOW NUMBER BET
+        If IsNumeric(txtBet.Text) Then
 
+            'reset btn
+            bytResult = 0
+            decRandom = 0
+            decBet = 0
+            rtbResults.Clear()
+            rtbNumber.Clear()
+
+            'get random number
+            decRandom = Rnd()
+            rtbResults.AppendText("Pre-calculation result is " & decRandom.ToString & vbNewLine) '<-debug tool
+
+            bytResult = Int((decRandom * 13) + 1)
+            'covert to card
+            rtbResults.AppendText("Post-Calculation result is " & bytResult.ToString & vbNewLine) '<-debug tool
+
+            'determine high/low
+            decBet = Convert.ToDecimal(txtBet.Text)
+            Select Case bytResult
+                Case <= 8
+                    rtbResults.AppendText("Nice guess, you win! +" & decBet.ToString & vbNewLine)
+                    rtbNumber.AppendText(bytResult.ToString & vbNewLine)
+                    decPoints = decPoints + decBet
+                Case >= 8
+                    rtbResults.AppendText("Sorry, you lose. -" & decBet.ToString & vbNewLine)
+                    rtbNumber.AppendText(bytResult.ToString & vbNewLine)
+                    decPoints = decPoints - decBet
+            End Select
+
+            'display new point value
+            lblPoints.Text = decPoints.ToString
+
+            'lose condition
+            If decPoints < 0 Then
+                btnHigh.Visible = False
+                btnLow.Visible = False
+                rtbResults.Clear()
+                rtbResults.AppendText("You got nothing to bet with! You lose!")
+            End If
+        Else
+            rtbResults.AppendText("Sorry, thats not a valid betting number." & vbNewLine)
+        End If
     End Sub
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
