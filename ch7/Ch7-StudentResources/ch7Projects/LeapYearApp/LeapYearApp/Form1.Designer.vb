dcae892eaa7e2a6fef6221291c69c12bda484873
JakeLaptop <jakejildefonso@gmail.com>
2018-11-06 10:42:57 -0700

    prelunch commit

--- ch7/Ch7-StudentResources/ch7Projects/LeapYearApp/LeapYearApp/Form1.Designer.vb
+++ ch7/Ch7-StudentResources/ch7Projects/LeapYearApp/LeapYearApp/Form1.Designer.vb
@@ -22,10 +22,82 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtYear = New System.Windows.Forms.TextBox()
+        Me.btnMethodA = New System.Windows.Forms.Button()
+        Me.btnMethod2 = New System.Windows.Forms.Button()
+        Me.lblOutput = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft YaHei", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(392, 26)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Enter a year to get the days in February"
+        '
+        'txtYear
+        '
+        Me.txtYear.Location = New System.Drawing.Point(17, 51)
+        Me.txtYear.Name = "txtYear"
+        Me.txtYear.Size = New System.Drawing.Size(100, 20)
+        Me.txtYear.TabIndex = 1
+        '
+        'btnMethodA
+        '
+        Me.btnMethodA.BackColor = System.Drawing.Color.Azure
+        Me.btnMethodA.Location = New System.Drawing.Point(123, 38)
+        Me.btnMethodA.Name = "btnMethodA"
+        Me.btnMethodA.Size = New System.Drawing.Size(112, 47)
+        Me.btnMethodA.TabIndex = 2
+        Me.btnMethodA.Text = "Method A"
+        Me.btnMethodA.UseVisualStyleBackColor = False
+        '
+        'btnMethod2
+        '
+        Me.btnMethod2.BackColor = System.Drawing.Color.Moccasin
+        Me.btnMethod2.Location = New System.Drawing.Point(241, 38)
+        Me.btnMethod2.Name = "btnMethod2"
+        Me.btnMethod2.Size = New System.Drawing.Size(112, 47)
+        Me.btnMethod2.TabIndex = 3
+        Me.btnMethod2.Text = "Method B"
+        Me.btnMethod2.UseVisualStyleBackColor = False
+        '
+        'lblOutput
+        '
+        Me.lblOutput.Font = New System.Drawing.Font("Microsoft YaHei", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblOutput.Location = New System.Drawing.Point(17, 88)
+        Me.lblOutput.Name = "lblOutput"
+        Me.lblOutput.Size = New System.Drawing.Size(336, 82)
+        Me.lblOutput.TabIndex = 4
+        Me.lblOutput.Text = "LIKE SCOOB THIS IS THE OUTPUT ZOINKS"
+        Me.lblOutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.lblOutput.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(412, 179)
+        Me.Controls.Add(Me.lblOutput)
+        Me.Controls.Add(Me.btnMethod2)
+        Me.Controls.Add(Me.btnMethodA)
+        Me.Controls.Add(Me.txtYear)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Leap Year Check - Jake Ildefonso"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtYear As TextBox
+    Friend WithEvents btnMethodA As Button
+    Friend WithEvents btnMethod2 As Button
+    Friend WithEvents lblOutput As Label
 End Class
--- ch7/Ch7-StudentResources/ch7Projects/LeapYearApp/LeapYearApp/Form1.vb
+++ ch7/Ch7-StudentResources/ch7Projects/LeapYearApp/LeapYearApp/Form1.vb
@@ -1,3 +1,21 @@
 ﻿Public Class Form1
+    Private Sub btnMethodA_Click(sender As Object, e As EventArgs) Handles btnMethodA.Click
+        'METHOD A - (Instructor description as follows) 
+        'This method will Get the year input by the user, then call a function that takes the year value as a parameter and returns an int for the number of days in February in that year. Assign that returned value to a variable in method A. Then call the sub that writes the result in the label. - REMEMBER TO USE MODULUS
 
+        If IsNumeric(txtYear.Text) Then 'user friendliness or whatever so no crashes, but i don't have any implimentation knowledge of how to check if its a decimal and reject it if is.
+            'CODE GOES HERE
+            Dim bytYear As Byte
+            txtYear.Text = bytYear
+
+
+        Else
+            lblOutput.Text = "Please input a number."
+        End If
+    End Sub
+
+    Private Sub btnMethod2_Click(sender As Object, e As EventArgs) Handles btnMethod2.Click
+        'METHOD B - (Instructor description as follows) 
+        'This method will Get the year input by the user, then calls a function that takes the year value as a parameter And determines if it Is a leap year, returning True Or False. In method B, call the function to check if the year Is a leap year, And If true, assign 29 To the number Of days, otherwise assign 28 to the number of days. Then call the sub that writes the result in the label. - REMEMBER TO USE MODULUS
+    End Sub
 End Class
