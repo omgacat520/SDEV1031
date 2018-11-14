86ddfca0b2498b17d2d62d8a9d613bd30629d083
JakeLaptop <jakejildefonso@gmail.com>
2018-11-14 10:35:03 -0700

    prelunch commit. Started ch8 Examples.

---
+++ ch8/ch8Examples/ReverseNameApp/ReverseNameApp/Form1.Designer.vb
@@ -0,0 +1,87 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.mtbInput = New System.Windows.Forms.MaskedTextBox()
+        Me.btnReverse = New System.Windows.Forms.Button()
+        Me.lblOutput = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Gadugi", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(146, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(135, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Enter a name"
+        '
+        'mtbInput
+        '
+        Me.mtbInput.Location = New System.Drawing.Point(101, 37)
+        Me.mtbInput.Name = "mtbInput"
+        Me.mtbInput.Size = New System.Drawing.Size(225, 20)
+        Me.mtbInput.TabIndex = 1
+        '
+        'btnReverse
+        '
+        Me.btnReverse.Location = New System.Drawing.Point(160, 63)
+        Me.btnReverse.Name = "btnReverse"
+        Me.btnReverse.Size = New System.Drawing.Size(100, 23)
+        Me.btnReverse.TabIndex = 2
+        Me.btnReverse.Text = "Reverse it!"
+        Me.btnReverse.UseVisualStyleBackColor = True
+        '
+        'lblOutput
+        '
+        Me.lblOutput.AutoSize = True
+        Me.lblOutput.Font = New System.Drawing.Font("Gadugi", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblOutput.Location = New System.Drawing.Point(182, 89)
+        Me.lblOutput.Name = "lblOutput"
+        Me.lblOutput.Size = New System.Drawing.Size(58, 19)
+        Me.lblOutput.TabIndex = 3
+        Me.lblOutput.Text = "Output"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(488, 289)
+        Me.Controls.Add(Me.lblOutput)
+        Me.Controls.Add(Me.btnReverse)
+        Me.Controls.Add(Me.mtbInput)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents mtbInput As MaskedTextBox
+    Friend WithEvents btnReverse As Button
+    Friend WithEvents lblOutput As Label
+End Class
---
+++ ch8/ch8Examples/ReverseNameApp/ReverseNameApp/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnReverse.Click
+        Dim strInput As String
+
+        strInput = mtbInput.Text
+
+
+
+        strInput.PadLeft()
+    End Sub
+End Class
