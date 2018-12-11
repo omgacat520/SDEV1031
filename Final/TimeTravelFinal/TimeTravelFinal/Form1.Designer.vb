72863ed3c52705375ee1f63556809f1c41649edf
JakeLaptop <jakejildefonso@gmail.com>
2018-12-11 10:46:22 -0700

    started the final
    started the 1031 final, and now I just gotta finish this bad boy up and get started on the next COURSE YEET

---
+++ Final/TimeTravelFinal/TimeTravelFinal/Form1.Designer.vb
@@ -0,0 +1,248 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.MaskedTextBox1 = New System.Windows.Forms.MaskedTextBox()
+        Me.MaskedTextBox2 = New System.Windows.Forms.MaskedTextBox()
+        Me.MaskedTextBox3 = New System.Windows.Forms.MaskedTextBox()
+        Me.MaskedTextBox4 = New System.Windows.Forms.MaskedTextBox()
+        Me.MaskedTextBox5 = New System.Windows.Forms.MaskedTextBox()
+        Me.MaskedTextBox6 = New System.Windows.Forms.MaskedTextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(117, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "wanna time travel, bro?"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Label2.Location = New System.Drawing.Point(12, 49)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(29, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Year"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Label3.Location = New System.Drawing.Point(12, 75)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(37, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Month"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Label4.Location = New System.Drawing.Point(12, 101)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(26, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Day"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Label5.Location = New System.Drawing.Point(13, 127)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(30, 13)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Hour"
+        '
+        'MaskedTextBox1
+        '
+        Me.MaskedTextBox1.Location = New System.Drawing.Point(55, 46)
+        Me.MaskedTextBox1.Mask = "0000"
+        Me.MaskedTextBox1.Name = "MaskedTextBox1"
+        Me.MaskedTextBox1.Size = New System.Drawing.Size(33, 20)
+        Me.MaskedTextBox1.TabIndex = 5
+        '
+        'MaskedTextBox2
+        '
+        Me.MaskedTextBox2.Location = New System.Drawing.Point(55, 72)
+        Me.MaskedTextBox2.Mask = "00"
+        Me.MaskedTextBox2.Name = "MaskedTextBox2"
+        Me.MaskedTextBox2.Size = New System.Drawing.Size(17, 20)
+        Me.MaskedTextBox2.TabIndex = 6
+        '
+        'MaskedTextBox3
+        '
+        Me.MaskedTextBox3.Location = New System.Drawing.Point(55, 98)
+        Me.MaskedTextBox3.Mask = "00"
+        Me.MaskedTextBox3.Name = "MaskedTextBox3"
+        Me.MaskedTextBox3.Size = New System.Drawing.Size(17, 20)
+        Me.MaskedTextBox3.TabIndex = 7
+        '
+        'MaskedTextBox4
+        '
+        Me.MaskedTextBox4.Location = New System.Drawing.Point(55, 124)
+        Me.MaskedTextBox4.Mask = "00"
+        Me.MaskedTextBox4.Name = "MaskedTextBox4"
+        Me.MaskedTextBox4.Size = New System.Drawing.Size(17, 20)
+        Me.MaskedTextBox4.TabIndex = 8
+        '
+        'MaskedTextBox5
+        '
+        Me.MaskedTextBox5.Location = New System.Drawing.Point(136, 98)
+        Me.MaskedTextBox5.Mask = "00"
+        Me.MaskedTextBox5.Name = "MaskedTextBox5"
+        Me.MaskedTextBox5.Size = New System.Drawing.Size(17, 20)
+        Me.MaskedTextBox5.TabIndex = 12
+        '
+        'MaskedTextBox6
+        '
+        Me.MaskedTextBox6.Location = New System.Drawing.Point(136, 72)
+        Me.MaskedTextBox6.Mask = "00"
+        Me.MaskedTextBox6.Name = "MaskedTextBox6"
+        Me.MaskedTextBox6.Size = New System.Drawing.Size(17, 20)
+        Me.MaskedTextBox6.TabIndex = 11
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.BackColor = System.Drawing.SystemColors.Highlight
+        Me.Label6.Location = New System.Drawing.Point(94, 101)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(30, 13)
+        Me.Label6.TabIndex = 10
+        Me.Label6.Text = "Hour"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.BackColor = System.Drawing.SystemColors.Highlight
+        Me.Label7.Location = New System.Drawing.Point(93, 75)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(26, 13)
+        Me.Label7.TabIndex = 9
+        Me.Label7.Text = "Day"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.BackColor = System.Drawing.SystemColors.Highlight
+        Me.Label8.Location = New System.Drawing.Point(94, 49)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(109, 13)
+        Me.Label8.TabIndex = 13
+        Me.Label8.Text = "How far in the future?" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(16, 145)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(163, 23)
+        Me.Button1.TabIndex = 14
+        Me.Button1.Text = "traverse the fabric of spacetime"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Label9.Location = New System.Drawing.Point(12, 28)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(66, 13)
+        Me.Label9.TabIndex = 15
+        Me.Label9.Text = "Todays date"
+        '
+        'Label10
+        '
+        Me.Label10.BackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.Label10.Location = New System.Drawing.Point(15, 171)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(188, 57)
+        Me.Label10.TabIndex = 16
+        Me.Label10.Text = "zis be mein output"
+        Me.Label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.Label10.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(220, 237)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.MaskedTextBox5)
+        Me.Controls.Add(Me.MaskedTextBox6)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.MaskedTextBox4)
+        Me.Controls.Add(Me.MaskedTextBox3)
+        Me.Controls.Add(Me.MaskedTextBox2)
+        Me.Controls.Add(Me.MaskedTextBox1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "spooky time travel"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents MaskedTextBox1 As MaskedTextBox
+    Friend WithEvents MaskedTextBox2 As MaskedTextBox
+    Friend WithEvents MaskedTextBox3 As MaskedTextBox
+    Friend WithEvents MaskedTextBox4 As MaskedTextBox
+    Friend WithEvents MaskedTextBox5 As MaskedTextBox
+    Friend WithEvents MaskedTextBox6 As MaskedTextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+End Class
--- Mod5Competency/Mod5Competency/Mod5Competency/Form1.vb
+++ Mod5Competency/Mod5Competency/Mod5Competency/Form1.vb
@@ -1,5 +1,37 @@
 ﻿Public Class Form1
     Private Sub btnExe_Click(sender As Object, e As EventArgs) Handles btnExe.Click
+        Dim intNumArray() As Integer = {0, 0, 0, 0, 0, 0} '6 array values, for each random number.
+        Dim intTemporary As Integer
 
+        For i = intNumArray.GetLowerBound(0) To intNumArray.GetUpperBound(0)
+            intNumArray(i) = Int(Rnd() * 100) ' assigns random integer value to each NumArray value.
+        Next
+
+        'sort array list from highest to lowest, making upper and lower bounds the largest and smallest.
+        For j = intNumArray.GetLowerBound(0) To intNumArray.GetUpperBound(0) - 1
+            For i = intNumArray.GetLowerBound(0) To intNumArray.GetUpperBound(0) - 1
+                If intNumArray(i) > intNumArray(i + 1) Then
+                    intTemporary = intNumArray(i)
+                    intNumArray(i) = intNumArray(i + 1)
+                    intNumArray(i + 1) = intTemporary
+                End If
+
+            Next
+        Next
+        lblNum1.Text = intNumArray(0).ToString
+        lblNum2.Text = intNumArray(1).ToString
+        lblNum3.Text = intNumArray(2).ToString
+        lblNum4.Text = intNumArray(3).ToString
+        lblNum5.Text = intNumArray(4).ToString
+        lblNum6.Text = intNumArray(5).ToString
+        lblBiggest.Text = "Biggest number is " & intNumArray(intNumArray.GetUpperBound(0)).ToString
+        lblSmallest.Text = "Smallest number is " & intNumArray(intNumArray.GetLowerBound(0)).ToString
+
+        lblBiggest.Visible = True
+        lblSmallest.Visible = True
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Randomize()
     End Sub
 End Class
