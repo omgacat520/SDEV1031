a1f6ebae5315685cf8ea590ea733de445487f850
JakeLaptop <jakejildefonso@gmail.com>
2018-12-13 14:45:44 -0700

    Simplified a lot of the code, fixed output.
    still need to sanitize user input, and simplify a bit more of the code, then we should be good to go.

--- Final/TimeTravelFinal/TimeTravelFinal/Form1.vb
+++ Final/TimeTravelFinal/TimeTravelFinal/Form1.vb
@@ -1,4 +1,7 @@
 ﻿Public Class Form1
+    'we could condense the two subs that provide the output arrays, by having a function based on a boolean value produced by finding out whether or not the year is a leap year, and if it is, it returns february as 29, if not, returns february as 28. Simplifies code, and makes it smaller.
+    Dim intFutureDateArray(3) As Integer
+
     Private Sub btnExe_Click(sender As Object, e As EventArgs) Handles btnExe.Click
         Dim intDateArray(3) As Integer
         intDateArray(0) = mtbThisHour.Text
@@ -10,38 +13,16 @@
         intDesiredFutureArray(0) = mtbFutureHour.Text
         intDesiredFutureArray(1) = mtbFutureDay.Text
 
-        Dim intFutureDateArray(3) As Integer
         intFutureDateArray = {0, 0, 0, 0}
-        ReDim intFutureDateArray(3) ' -- Hour, Day, Month, Year values.
 
-        'for data validation, mtb prevents anything but integer values, and this For and If statement assures that all inputs make sense.
-        For i = intDateArray.GetLowerBound(0) To (intDateArray.GetUpperBound(0))
-            If intDateArray(i) < 0 Then 'make sure input exists for DateArray
-                MessageBox.Show("Make sure all text boxes are filled out.", "Try again", MessageBoxButtons.OK, MessageBoxIcon.Error)
 
-            ElseIf intDateArray(2) > 12 Or intDateArray(2) < 0 Then 'validates months
-                MessageBox.Show("Your months must be between 1 and 12", "Try again", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        'NEED TO REMAKE DATA VALIDATION
 
-            ElseIf intDateArray(0) > 24 Or intDateArray(1) <= -1 Then 'validates days
-                MessageBox.Show("Your Hours must be between 0 and 24", "Try again", MessageBoxButtons.OK, MessageBoxIcon.Error)
-            Else
-                For c = intDesiredFutureArray.GetLowerBound(0) To intDesiredFutureArray.GetUpperBound(0)
-                    If intDesiredFutureArray(c) < 0 Then 'make sure input exists for Desired Future Array
-                        MessageBox.Show("Make sure all text boxes are filled out.", "Try again", MessageBoxButtons.OK, MessageBoxIcon.Error)
-                    ElseIf intDateArray(2) > 12 Or intDesiredFutureArray(1) < 0 Then 'validates months
-                        MessageBox.Show("Your Desired months must be between 1 and 12", "Try again", MessageBoxButtons.OK, MessageBoxIcon.Error)
-                    ElseIf intDateArray(0) > 24 Or intDesiredFutureArray(0) <= -1 Then 'validates days
-                        MessageBox.Show("Your Desired Hours must be between 0 and 24", "Try again", MessageBoxButtons.OK, MessageBoxIcon.Error)
-                    Else
-                        'CODE GOES HERE
-                        Call DoTheStuff(intDateArray, intDesiredFutureArray, intFutureDateArray)
-                        Call subOutput(intDesiredFutureArray)
-                    End If
-                Next
-            End If
-        Next
+        'CODE GOES HERE
+        Call DoTheStuff(intDateArray, intDesiredFutureArray, intFutureDateArray)
+        Call subOutput(intFutureDateArray)
     End Sub
-    Private Function DoTheStuff(ByVal IntDateArray() As Integer, ByVal IntDesiredFutureArray() As Integer, ByVal intFutureDateArray() As Integer)
+    Private Sub DoTheStuff(ByVal IntDateArray() As Integer, ByVal IntDesiredFutureArray() As Integer, ByVal intFutureDateArray() As Integer)
 
         Dim boolIsLeap As Boolean ' this determines whether its a leap year, and then defines it for future code
         If IntDateArray(3) Mod 4 = 0 Then
@@ -55,133 +36,46 @@
         Else 'has 28 days in february
             Call NoLeapYear(IntDateArray, IntDesiredFutureArray, intFutureDateArray)
         End If
+    End Sub
 
-    End Function
-
-    Private Function HasLeapYear(ByVal IntDateArray() As Integer, ByVal IntDesiredFutureArray() As Integer, ByVal intFutureDateArray() As Integer)
+    Private Sub HasLeapYear(ByVal IntDateArray() As Integer, ByVal IntDesiredFutureArray() As Integer, ByVal intFutureDateArray() As Integer)
         'THIS ONE INCLUDES LEAP YEARS
-        'instead of breaking down, just compare units to maximum values, and if they exceed maximums, then add to next largest denomination, like a cash register.
-        'leap year hours = 8784, normal year hours = 8760
+        'compare units to maximum values, and if they exceed maximums, then add to next largest denomination, like a cash register.
         'Depending on the month, take day value (24 hrs) and multiply by number of days.
         'Link for how many days: https://www.timeanddate.com/calendar/months/
 
         'Might need to change when this is assigned, testing needed for these.
-        intFutureDateArray(0) = IntDateArray(0) + IntDesiredFutureArray(0)
-        intFutureDateArray(1) = IntDateArray(1) + IntDesiredFutureArray(1) + intFutureDateArray(1)
-        intFutureDateArray(2) = IntDateArray(2) + intFutureDateArray(2)
-        intFutureDateArray(3) = IntDateArray(3) + intFutureDateArray(3)
-
+        intFutureDateArray(0) = IntDateArray(0) + IntDesiredFutureArray(0) 'hours
+        intFutureDateArray(1) = IntDateArray(1) + IntDesiredFutureArray(1) + intFutureDateArray(1) ' days
+        intFutureDateArray(2) = IntDateArray(2) + intFutureDateArray(2) ' months
+        intFutureDateArray(3) = IntDateArray(3) + intFutureDateArray(3) ' years
+        'day value
         If intFutureDateArray(0) > 24 Then
             Do Until intFutureDateArray(0) < 24
                 intFutureDateArray(1) = intFutureDateArray(1) + 1
                 intFutureDateArray(0) = intFutureDateArray(0) - 24
             Loop
         End If
-
-        Dim j As Integer = 1
-        For i = 0 To j
-            Select Case intFutureDateArray(2) 'need to figure out what month it is, so I know when to switch from X ammount of days, to 0 and then add 1 to the month value.
-                Case Is = 1
-                    If intFutureDateArray(1) > 31 Then
-                        Do Until intFutureDateArray(1) < 31
-                            intFutureDateArray(2) = intFutureDateArray(2) + 1
-                            intFutureDateArray(1) = intFutureDateArray(1) - 31
-                        Loop
-                        j = j + 1
-                    End If
-                Case Is = 2
-                    If intFutureDateArray(1) > 29 Then
-                        Do Until intFutureDateArray(1) < 29
-                            intFutureDateArray(2) = intFutureDateArray(2) + 1
-                            intFutureDateArray(1) = intFutureDateArray(1) - 29
-                        Loop
-                        j = j + 1
-                    End If
-                Case Is = 3
-                    If intFutureDateArray(1) > 31 Then
-                        Do Until intFutureDateArray(1) < 31
-                            intFutureDateArray(2) = intFutureDateArray(2) + 1
-                            intFutureDateArray(1) = intFutureDateArray(1) - 31
-                        Loop
-                        j = j + 1
-                    End If
-                Case Is = 4
-                    If intFutureDateArray(1) > 30 Then
-                        Do Until intFutureDateArray(1) < 30
-                            intFutureDateArray(2) = intFutureDateArray(2) + 1
-                            intFutureDateArray(1) = intFutureDateArray(1) - 30
-                        Loop
-                        j = j + 1
-                    End If
-                Case Is = 5
-                    If intFutureDateArray(1) > 31 Then
-                        Do Until intFutureDateArray(1) < 31
-                            intFutureDateArray(2) = intFutureDateArray(2) + 1
-                            intFutureDateArray(1) = intFutureDateArray(1) - 31
-                        Loop
-                        j = j + 1
-                    End If
-                Case Is = 6
-                    If intFutureDateArray(1) > 30 Then
-                        Do Until intFutureDateArray(1) < 30
-                            intFutureDateArray(2) = intFutureDateArray(2) + 1
-                            intFutureDateArray(1) = intFutureDateArray(1) - 30
-                        Loop
-                        j = j + 1
-                    End If
-                Case Is = 7
-                    If intFutureDateArray(1) > 31 Then
-                        Do Until intFutureDateArray(1) < 31
-                            intFutureDateArray(2) = intFutureDateArray(2) + 1
-                            intFutureDateArray(1) = intFutureDateArray(1) - 31
-                        Loop
-                        j = j + 1
-                    End If
-                Case Is = 8
-                    If intFutureDateArray(1) > 31 Then
-                        Do Until intFutureDateArray(1) < 31
-                            intFutureDateArray(2) = intFutureDateArray(2) + 1
-                            intFutureDateArray(1) = intFutureDateArray(1) - 31
-                        Loop
-                        j = j + 1
-                    End If
-                Case Is = 9
-                    If intFutureDateArray(1) > 30 Then
-                        Do Until intFutureDateArray(1) < 30
-                            intFutureDateArray(2) = intFutureDateArray(2) + 1
-                            intFutureDateArray(1) = intFutureDateArray(1) - 30
-                        Loop
-                        j = j + 1
-                    End If
-                Case Is = 10
-                    If intFutureDateArray(1) > 31 Then
-                        Do Until intFutureDateArray(1) < 31
-                            intFutureDateArray(2) = intFutureDateArray(2) + 1
-                            intFutureDateArray(1) = intFutureDateArray(1) - 31
-                        Loop
-                        j = j + 1
-                    End If
-                Case Is = 11
-                    If intFutureDateArray(1) > 30 Then
-                        Do Until intFutureDateArray(1) < 30
-                            intFutureDateArray(2) = intFutureDateArray(2) + 1
-                            intFutureDateArray(1) = intFutureDateArray(1) - 30
-                        Loop
-                        j = j + 1
-                    End If
-                Case Is = 12
-                    If intFutureDateArray(1) > 31 Then
-                        Do Until intFutureDateArray(1) < 31
-                            intFutureDateArray(2) = intFutureDateArray(2) + 1
-                            intFutureDateArray(1) = intFutureDateArray(1) - 31
-                        Loop
-                        j = j + 1
-                    End If
+        'month value
+        Dim intWhatMonth As Integer 'temporary variable
+        Select Case intFutureDateArray(2)
+                'Checks how many days per month
+                Case Is = 1 Or 3 Or 5 Or 7 Or 8 Or 10 Or 12 '31 day months
+                    intWhatMonth = 31
+                Case Is = 2 'february / leap year month
+                    intWhatMonth = 29
+                Case Is = 4 Or 6 Or 9 Or 11 '30 day months
+                    intWhatMonth = 30
             End Select
-        Next
+
+        If intFutureDateArray(1) > intWhatMonth Then
+            Do Until intFutureDateArray(1) <= intWhatMonth
+                intFutureDateArray(2) = intFutureDateArray(2) + 1
+                intFutureDateArray(1) = intFutureDateArray(1) - intWhatMonth
+            Loop
+        End If
         'Year value
-        j = 1
-        intFutureDateArray(3) = intFutureDateArray(3) + IntDateArray(3)
+        Dim j As Integer = 1
         For i = 0 To j
             If intFutureDateArray(2) > 12 Then
                 Do Until intFutureDateArray(3) > 12
@@ -192,18 +86,17 @@
             End If
         Next
 
-        Return intFutureDateArray(0)
-        Return intFutureDateArray(1)
-        Return intFutureDateArray(2)
-        Return intFutureDateArray(3)
-    End Function
+        Debug.WriteLine("returning with " & intFutureDateArray(0) & " As hours and " & intFutureDateArray(1) & " as days " & intFutureDateArray(2) & " as months and " & intFutureDateArray(3) & " as years")
+
+    End Sub
 
     Private Function NoLeapYear(ByVal IntDateArray() As Integer, ByVal IntDesiredFutureArray() As Integer, ByVal intFutureDateArray() As Integer)
 
     End Function
-    Private Sub subOutput(ByVal intFutureDateArray() As Integer)
-        ReDim Preserve intFutureDateArray(3)
-        lblOutput.Text = (intFutureDateArray(0) & " Military Time on " & intFutureDateArray(2) & "/" & intFutureDateArray(1) & "/" & intFutureDateArray(3))
+
+    Private Sub subOutput(intAnotherFutureDateArray() As Integer)
+        lblOutput.Text = (intAnotherFutureDateArray(0) & ":00 Military Time on " & intAnotherFutureDateArray(2) & "/" & intAnotherFutureDateArray(1) & "/" & intAnotherFutureDateArray(3))
+        Debug.WriteLine("Finish with a intFutureDate array of " & intAnotherFutureDateArray(0) & " Hours, " & intAnotherFutureDateArray(1) & " Days, " & intAnotherFutureDateArray(2) & " Months, " & intAnotherFutureDateArray(3) & " Years.")
         lblOutput.Visible = True
     End Sub
 End Class
